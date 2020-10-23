8e95babc9d7699ee04bcf3cf5d8eba71b33b3dbd
Angel Martínez <andmartinezci@unal.edu.co>
2020-10-23 08:35:25 -0500

    Update pluto_image_example
    Update on image definition

--- Python/pluto_image_example.py
+++ Python/pluto_image_example.py
@@ -14,11 +14,17 @@ else:
   os.system('mkdir IMAGES')
 
 #- For a 3D image (for a 2D, dim=2):
-pi.image(2, cmap='jet', aspect='auto', dim=3, dslice='13',n=55) 
-#plt.xticks(np.linspace(0,1e4,5), np.linspace(-5,5,5,dtype=int))
-#plt.yticks(np.linspace(0,2e5,5), np.linspace(0,200,5,dtype=int))
-plt.xlabel('x [km]')
-plt.ylabel('Depth [km]')
+xr, yr = pi.image(2, cmap='jet', aspect='auto', dim=3, dslice='13',n=55) 
+xran = [np.min(xr),np.max(xr)]
+yran = [np.min(yr),np.max(yr)]
+XX0 = np.linspace(xran[0],xran[1],9)
+XX1 = np.linspace(-5,5,9,dtype=int)
+YY0 = np.linspace(yran[0],yran[1],9)
+YY1 = np.linspace(100,0,9,dtype=int)
+plt.xticks(XX0,XX1)
+plt.yticks(YY0,YY1)
+#plt.xlabel('x [km]')
+#plt.ylabel('Depth [km]')
 
 string = '%0.4d' % (2)  # To save custom frame with name pic.000#.png
 #plt.savefig('IMAGES/pic.'+string+'.png')
